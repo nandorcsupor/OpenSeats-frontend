b3b22603cf0d786d9ac6b13777780a67e6a16ac0
nandorcsupor <csuprok@hotmail.com>
2023-05-21 12:47:44 +0200

    add landing page and routing and svgs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,29 @@
-import Head from "next/head"
-import { Button } from "components/Button/Button"
-import { LP_GRID_ITEMS } from "../lp-items"
+import Head from "next/head";
+import { Button } from "components/Button/Button";
+import { CARDS } from "cards";
 
 export default function Web() {
   return (
     <>
       <Head>
-        <title>Next.js Enterprise Boilerplate</title>
+        <title>OpenSeats - Where True Fans Buy Their Tickets</title>
       </Head>
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-purple-500">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
-              Next.js Enterprise Boilerplate
-            </h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Jumpstart your enterprise project with our feature-packed, high-performance Next.js boilerplate!
-              Experience rapid UI development, AI-powered code reviews, and an extensive suite of tools for a smooth and
-              enjoyable development process.
-            </p>
-            <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
-              Get started
+            <div className="prose prose-lg max-w-none mx-auto text-white font-body pt-5">
+              <h1 className="mb-4 text-5xl font-extrabold leading-none tracking-tight md:text-7xl xl:text-8xl">
+                OpenSeats - Where True Fans Buy Their Tickets
+              </h1>
+              <p className="mb-6 font-light text-lg lg:mb-8 lg:text-xl pt-12">
+                Welcome to OpenSeats, the game-changer in the ticketing industry. Leveraging the power of blockchain technology, we've pioneered a new era of transparent and fair ticket sales. OpenSeats ensures not just authenticity but also enforces a cap on resale prices, meaning every fan has a fair shot at experiencing their dream events. No longer will fans be held ransom by inflated resale prices; our unique platform ensures tickets can be resold only once and at no more than a 5% markup. Bid farewell to ticket scalping and embrace the future of ticketing with OpenSeats - where true fans buy their tickets.
+              </p>
+            </div>
+            <Button className="mr-3 bg-yellow-400 text-black" href={"/start-match"}>
+              Start Match
             </Button>
-            <Button
-              href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
-              intent="secondary"
-            >
-              Deploy Now
+            <Button className="bg-yellow-400 text-black" href={"/available-matches"}>
+              See Available Matches
             </Button>
           </div>
         </div>
@@ -34,7 +31,7 @@ export default function Web() {
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
-            {LP_GRID_ITEMS.map((singleItem) => (
+            {CARDS.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
                 <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
                   {singleItem.icon}
@@ -46,6 +43,7 @@ export default function Web() {
           </div>
         </div>
       </section>
+
     </>
-  )
+  );
 }
