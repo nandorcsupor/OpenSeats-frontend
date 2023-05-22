f1eb4df4f6791bdeffbdb21d7fe20cb061b071a9
nandorcsupor <csuprok@hotmail.com>
2023-05-22 15:49:24 +0200

    color transition and fixed icons

---
+++ pages/icons/cards2.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import BlockchainIcon from "./blockchainicon"
+import SecurityIcon from "./securityicon"
+import BusinessIcon from "./businessicon"
+
+interface CardsData {
+  title: string
+  description: string
+  icon: React.ReactNode
+}
+
+const IconData: CardsData[] = [
+  {
+    title: "Blockchain technology",
+    description:
+      "Transformative by nature, blockchain technology ensures unparalleled transparency and security in ticketing. Powered by an architecture that is optimized for speed, performance, and reliability, we guarantee a seamless and efficient ticket buying experience.",
+    icon: <BlockchainIcon width={300} height={150} />,
+  },
+  {
+    title: "Security",
+    description:
+      "The immutability and transparency of blockchain ensure the integrity of data and protect against unauthorized tampering, providing enhanced safety in various domains.",
+    icon: <SecurityIcon width={300} height={150} />,
+  },
+  {
+    title: "Fair Business",
+    description:
+      "Blockchain empowers businesses with fairness by eliminating intermediaries and enabling direct peer-to-peer transactions. Its decentralized nature ensures transparency, immutability, and equal access, fostering a level playing field for all participants.",
+    icon: <BusinessIcon width={300} height={150} />,
+  },
+]
+
+export default IconData
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 import { Button } from "components/Button/Button"
-import { CARDS } from "cards"
+import IconData from "./icons/cards2"
 
 export default function Web() {
   return (
@@ -8,7 +8,7 @@ export default function Web() {
       <Head>
         <title>OpenSeats - Where True Fans Buy Their Tickets</title>
       </Head>
-      <section className="bg-purple-500">
+      <section className="bg-gradient-to-b from-purple-800 via-purple-700 to-white transition-all duration-1000">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
             <div className="prose prose-lg mx-auto max-w-none pt-5 font-body text-white">
@@ -33,13 +33,13 @@ export default function Web() {
           </div>
         </div>
       </section>
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-white transition-all duration-1000 dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
           <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
-            {CARDS.map((singleItem) => (
+            {IconData.map((singleItem) => (
               <div key={singleItem.title} className="flex flex-col items-center justify-center text-center">
-                <div className="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900 lg:h-12 lg:w-12">
-                  {singleItem.icon}
+                <div className="mb-4 flex items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900">
+                  <div style={{ width: "100%", height: "100%" }}>{singleItem.icon}</div>
                 </div>
                 <h3 className="mb-2 text-xl font-bold dark:text-white">{singleItem.title}</h3>
                 <p className="text-gray-500 dark:text-gray-400">{singleItem.description}</p>
