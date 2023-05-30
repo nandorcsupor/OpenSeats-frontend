551d62eb24f15bd10410dc3e877c5a1129e8b19c
nandorcsupor <csuprok@hotmail.com>
2023-05-30 21:04:32 +0200

    display available matches

--- pages/available-matches/index.tsx
+++ pages/available-matches/index.tsx
@@ -1,39 +1,40 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import { Button } from "components/Button/Button";
 
 interface Match {
-  id: number;
-  title: string;
-  date: string;
-  ticketsAvailable: number;
+  ticket_address: string;
+  match_name: string;
+  ticket_date: string;
+  max_tickets: number;
 }
 
 export default function Web() {
   const [matchData, setMatchData] = useState<Match[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchMatchData = async () => {
-      try {
-        const response = await fetch("https://fakeapi.com/matches"); // Replace with your API endpoint URL
-        const data = await response.json();
-        setMatchData(data as Match[]);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching match data:", error);
-      }
-    };
+  const fetchMatchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/get-matches");
+      const data = await response.json();
+      setMatchData(data as Match[]);
+    } catch (error) {
+      console.error("Error fetching match data:", error);
+    }
+  };
 
-    // Simulating API call with setTimeout
-    setTimeout(fetchMatchData, 2000);
+  useEffect(() => {
+    const fetchMatchDataInterval = setInterval(fetchMatchData, 5000);
+
+    // Clean up interval on component unmount
+    return () => clearInterval(fetchMatchDataInterval);
   }, []);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [matchData]);
 
   return (
     <>
-      <Head>
-        <title>OpenSeats - Where True Fans Buy Their Tickets</title>
-      </Head>
       <section className="bg-gradient-to-b from-purple-800 via-purple-700 to-white transition-all duration-1000">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
@@ -47,29 +48,49 @@ export default function Web() {
       </section>
       <section className="bg-white transition-all duration-1000 dark:bg-gray-900">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
-          <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+            {/* Render match data table */}
             {isLoading ? (
               <div className="flex flex-col items-center justify-center text-center w-full">
                 <div className="mb-4 font-bold">LOADING</div>
-                {/* Additional styling */}
+              </div>
+            ) : matchData.length === 0 ? (
+              <div className="flex flex-col items-center justify-center text-center w-full">
+                <div className="mb-4">No match data available.</div>
               </div>
             ) : (
-              // Render match data
-              matchData.map((match) => (
-                <div key={match.id} className="flex flex-col items-center justify-center text-center">
-                  <div className="mb-4 flex items-center justify-center rounded-full bg-primary-100 p-1.5 text-blue-700 dark:bg-primary-900">
-                    <div style={{ width: "100%", height: "100%" }}>{/* Render your icon here */}</div>
-                  </div>
-                  <h3 className="mb-2 text-xl font-bold dark:text-white">{match.title}</h3>
-                  <p className="text-gray-500 dark:text-gray-400">
-                    {match.date} - {match.ticketsAvailable} tickets available
-                  </p>
-                </div>
-              ))
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Ticket Address
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Match Name
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Ticket Date
+                    </th>
+                    <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                      Max Tickets
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {matchData.map((match) => (
+                    <tr key={match.ticket_address}>
+                      <td className="px-6 py-4 whitespace-nowrap">{match.ticket_address}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{match.match_name}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{match.ticket_date}</td>
+                      <td className="px-6 py-4 whitespace-nowrap">{match.max_tickets}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             )}
           </div>
         </div>
       </section>
     </>
-  );
+  )
 }
